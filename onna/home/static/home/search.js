b76508c4dfbdb086159a1ff989b6d194d822e20c
Dinmukhametov Salavat <salavatarus@gmail.com>
2019-03-29 19:01:13 +0300

    working search amount of cars

--- onna/home/static/home/search.js
+++ onna/home/static/home/search.js
@@ -1,17 +1,32 @@
-var search_field = document.querySelector('#search_input_field')
-var search_button = document.querySelector('#search_button')
+var input_mark = document.getElementById('input_mark');
+var input_model = document.getElementById('input_model');
+var input_year_to = document.getElementById('input_year_to')
+var input_year_from = document.getElementById('input_year_from')
+var input_eng_capacity_from = document.getElementById('eng_capacity_from')
+var input_eng_capacity_to = document.getElementById('eng_capacity_to')
+var input_mileage_from = document.getElementById('mileage_from')
+var input_mileage_to = document.getElementById('mileage_to')
+var input_transmission_type = document.getElementById('trainsmission')
+var search_button = document.getElementById('search_button')
+var search_result = document.getElementById('search_result')
 
 search_button.addEventListener('click', function(){
     $.ajax({
         url:'query/',
         data:{
-            'select':search_field.value
+            'mark':input_mark.value,
+            'model':input_model.value,
+            'year':[input_year_from.value, input_year_to.value],
+            'engine':[input_eng_capacity_from.value, input_eng_capacity_to.value],
+            'millage':[input_mileage_from.value, input_mileage_to.value],
+            'kpp':input_transmission_type.value
         },
         dataType: 'json',
         success:function(data){
-            if(data.is_taken){
-                console.log("seccess: "+search_field.value)
-            }
+            search_result.innerHTML = data['response']
+        },
+        fail:function(data){
+            search_result.innerHTML = "Cannot find"
         }
     });
 });
--- onna/home/views.py
+++ onna/home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
-
+from bs4 import BeautifulSoup as bf
+import urllib3
 
 
 def index(request):
@@ -10,12 +11,71 @@ def index(request):
 
 def query(request):
     qr = request.GET.get('query', None)
+    print(qr)
     data = {
-        'is_taken':True
+        'response':get_data_from_avito(qr)
     }
     return JsonResponse(data)
 
+def get_data_from_avito(params):
+    http = urllib3.PoolManager()
+    answer = ''
 
+    avito_url = get_avito_url(params)
+    r = http.request('GET', avito_url)
+    soup = bf(r.data, 'html.parser')
+    answer = len([price.text for price in soup.find_all('span', {"class": "price"})])
+    return answer
+
+def get_avito_url(prm):
+    base = 'https://www.avito.ru/rossiya/avtomobili'
+    try:
+        base += "/"+ prm['mark']
+    except (KeyError, TypeError):
+        pass
+    try:
+        base += '/'+prm['model']
+    except (KeyError, TypeError):
+        pass
+    base += '?s_trg=4&f='
+    try:
+        base += get_type_range_url('body', prm['body'])
+    except (KeyError, TypeError):
+        pass
+    try:
+        base += '.'+get_range_url('year',prm['year'])
+    except (KeyError, TypeError):
+        pass
+    try:
+        base += '.'+get_type_range_url('kpp', prm['kpp'])
+    except (KeyError, TypeError):
+        pass
+    try:
+        base += '.'+get_range_url('engine', prm['engine'])
+    except (KeyError, TypeError):
+        pass
+    return base
+
+def get_range_url(name, prms):
+    base = multiple_params[name]['base']
+    all_params = list(multiple_params[name])
+    a1, a2=-1,-1
+    for pr in all_params:
+        if pr != 'base':
+            if float(prms[0])<=float(pr):
+                a1 = multiple_params[name][pr]
+                break
+    for pr in all_params:
+        if pr != 'base':
+            if float(prms[1])<=float(pr):
+                a2 = multiple_params[name][pr]
+                break
+    return str(base)+'_'+str(a1)+'b'+str(a2)
+
+def get_type_range_url(name, prms):
+    first, second = multiple_params[name][prms[0]], multiple_params[name][prms[1]]
+    base = multiple_params[name]['base']
+    return str(base)+'_'+str(first)+'-'+str(second)
 
 
 
